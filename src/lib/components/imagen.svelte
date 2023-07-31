9cb66891a89e70add9f02bfe7a3e49e64007780a
Joel Mora <joeljmorab@gmail.com>
2023-07-31 13:16:03 -0400

    peleando con las imagenes

---
+++ src/lib/components/imagen.svelte
@@ -0,0 +1,8 @@
+<script>
+    // import Cara from '../../routes/resourse/sin fondo.png'
+    import Cara from '../../../static/imagenes/sin fondo.png'
+</script>
+<div class="">
+
+</div>
+<img class="" alt="Joel Mora" src={Cara}>
---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,12 @@
+<div>
+    <nav>
+        <ul class="flex flex-col justify-center gap">
+            <li>Joel mora.</li>
+            <li>Sobre mi.</li>
+            <li>Portafolio.</li>
+            <li>Educacion.</li>
+            <li>Skills.</li>
+            <li>Contacto.</li>
+        </ul>
+    </nav>
+</div>
