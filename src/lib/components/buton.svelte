254fe0595810fd183d17d847f7a029d90a93705c
Joel Mora <joeljmorab@gmail.com>
2023-07-31 22:38:20 -0400

    pagina 1 completa

---
+++ src/lib/components/buton.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let texto = '';
+    export let hovercolor = '';
+</script>
+
+<button class="rounded-xl bg-verde text-xl w-60 h-14 cursor-pointer transition duration-200 ease-in-out active:scale-95 hover:bg-{hovercolor}">
+{texto}
+</button>
