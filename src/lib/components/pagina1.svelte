4dc5240a36953022abfa50a63777b5998783847b
Joel Mora <joeljmorab@gmail.com>
2023-08-03 20:38:00 -0400

    cambios esteticos de produccion

--- src/lib/components/pagina1.svelte
+++ src/lib/components/pagina1.svelte
@@ -3,15 +3,15 @@
     // export let cualquiercosa;
 </script>
 <section class="w-5/6 ml-izq h-screen flex flex-col pl-11 justify-center gap-20 items-center pr-11">
-    <div class="flex mb-">
+    <div class="flex">
         <h1 class="text-rosa text-9xl font-semibold uppercase pr-7 ">JOEL</h1>
         <h1 class="text-verde text-9xl font-semibold uppercase">MORA</h1>
     </div>
     <h2 class="text-subt text-4xl">Ingeniero, Análisis de datos, desarrollador.</h2>
     <h2 class="text-azul text-3xl">Portafolio personal digital.</h2>
     <span class="flex mb-6">
-        <h3 class="pr-1 text-2xl text-azul">Si quieres ponerte en contacto</h3>
-        <a class="text-xl text-verde" href="mailto:JoelJMoraB@gmail.com">JoelJMoraB@gmail.com</a>
+        <h3 class="pr-1 text-3xl text-azul">Si quieres ponerte en contacto</h3>
+        <a class="text-3xl text-verde" href="mailto:JoelJMoraB@gmail.com">JoelJMoraB@gmail.com</a>
         <!-- <h3 class="text-xl text-verde">JoelJMoraB@gmail.com.</h3> -->
     </span>
     <button class="btn-normal">Descargar mi CV</button>
