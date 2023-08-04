2f2fb5e2e1fbaac51a5cfe5aa9b834b72cbf2db4
Joel Mora <joeljmorab@gmail.com>
2023-08-04 16:10:36 -0400

    listo hasta contacto, se crea nueva rama para probar el daisyui

---
+++ src/lib/components/pagina6.svelte
@@ -0,0 +1,47 @@
+<script>
+
+</script>
+
+
+<section class="w-5/6 ml-izq flex flex-col h-screen justify-center gap-10 px-32">
+    <div class="flex">
+        <h1 class="text-rosa text-8xl font-bold uppercase pr-7 tracking-widest ">Skills.</h1>
+    </div>
+    <div class="flex flex-col text-subt text-3xl gap-10 font-medium">
+
+        <div class="flex flex-col space-y-5 mt-3">
+            <h1 class="">Lenguajes de programación</h1>
+            <span class="flex space-x-5">
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+            </span>    
+        </div>
+        
+        <div class="flex flex-col space-y-5 mt-3">
+            <h1 class="">Frameworks y tecnologias</h1>    
+            <span class="flex space-x-5">
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+            </span>    
+        </div>
+        
+        <div class="flex flex-col space-y-5 mt-3">
+            <h1 class="">Frameworks y tecnologias</h1>    
+            <span class="flex space-x-5">
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
+            </span>    
+        </div>
+    </div>
+
+</section>
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
     import Page3 from '../lib/components/pagina3.svelte'
     import Page4 from '../lib/components/pagina4.svelte'
     import Page5 from '../lib/components/pagina5.svelte'
+    import Page6 from '../lib/components/pagina6.svelte'
 </script>
 
 
@@ -15,5 +16,6 @@
     <Page3 />
     <Page4 />
     <Page5 />
+    <Page6 />
 </div>
 
