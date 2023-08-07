b219cc20280a5c458617ac657fa23f2521474f36
Joel Mora <joeljmorab@gmail.com>
2023-08-07 17:41:44 -0400

    daisyui funcionando perfectamente

--- src/lib/components/pagina6.svelte
+++ src/lib/components/pagina6.svelte
@@ -1,43 +1,26 @@
 <script>
-
+    let margenTxCj = '6' 
 </script>
 
 
 <section class="w-5/6 ml-izq flex flex-col h-screen justify-center gap-10 px-32">
     <div class="flex">
-        <h1 class="text-rosa text-8xl font-bold uppercase pr-7 tracking-widest ">Skills.</h1>
+        <h1 class="text-rosa text-8xl font-bold uppercase pr-7 tracking-widest ">Contacto.</h1>
     </div>
-    <div class="flex flex-col text-subt text-3xl gap-10 font-medium">
-
-        <div class="flex flex-col space-y-5 mt-3">
-            <h1 class="">Lenguajes de programación</h1>
-            <span class="flex space-x-5">
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-            </span>    
-        </div>
+    <div class="flex flex-col text-subt text-2xl gap-10 font-medium w-screen">
+        <span class="w-full space-y-{margenTxCj}}">
+            <h3>Nombre:</h3>
+            <input type="text" placeholder="Escribe aca tu nombre" class="input text-lg input-bordered input-primary w-full max-w-xl" >
+        </span>
+        <span class="w-full space-y-2">
+            <h3>Correo:</h3>
+            <input type="text" placeholder="Escribe aca tu correo" class="input text-lg input-bordered input-primary w-full max-w-xl" >
+        </span>
+        <span class="w-full space-y-2">
+            <h3>Mensaje:</h3>
+            <textarea class="textarea textarea-primary w-full max-w-xl h-48 text-lg" placeholder="Escribe aca tu mensaje"></textarea>
+        </span>
         
-        <div class="flex flex-col space-y-5 mt-3">
-            <h1 class="">Frameworks y tecnologias</h1>    
-            <span class="flex space-x-5">
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-            </span>    
-        </div>
-        
-        <div class="flex flex-col space-y-5 mt-3">
-            <h1 class="">Frameworks y tecnologias</h1>    
-            <span class="flex space-x-5">
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-                <button class="btn-normal w-fit h-fit p-1">tecnologia</button>
-            </span>    
-        </div>
     </div>
 
 </section>
