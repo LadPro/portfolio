aa286dd678147862beb3f7a7a503d8191821dd87
Joel Mora <joeljmorab@gmail.com>
2023-08-24 21:22:16 -0400

    provando el formulario

--- src/lib/components/pagina6.svelte
+++ src/lib/components/pagina6.svelte
@@ -13,21 +13,23 @@ let mensaje = ''
     </div>
     <div class="lg:grid grid-cols-2 falex w-full items-cenater h-full ">
 
-        <div class="flex flex-col text-subt text-4xl lg:text-2xl gap-10 font-medium px-2">
+        <form action="https://formsubmit.co/joeljmorab@gmail.com" method="POST" class="flex flex-col text-subt text-4xl lg:text-2xl gap-10 font-medium px-2">
             <span class="w-full space-y-2">
                 <h3>Nombre:</h3>
-                <input bind:value={nombre} type="text" placeholder="Escribe aca tu nombre" class="input text-4xl max-lg:h-20 lg:text-lg border-gray-400 focus:outline-verde border-4 rounded-2xl w-full lg:max-w-xl" >
+                <input  bind:value={nombre} type="nombre" name="nombre" placeholder="Escribe aca tu nombre" class="input text-4xl max-lg:h-20 lg:text-lg border-gray-400 focus:outline-verde border-4 rounded-2xl w-full lg:max-w-xl" >
             </span>
             <span class="w-full space-y-2">
                 <h3>Correo:</h3>
-                <input bind:value={email} type="text" placeholder="Escribe aca tu correo" class="input text-4xl max-lg:h-20 lg:text-lg input-bordered border-gray-400 focus:outline-verde border-4 rounded-2xl input-primary w-full lg:max-w-xl" >
+                <input bind:value={email} type="email" name="email" placeholder="Escribe aca tu correo" class="input text-4xl max-lg:h-20 lg:text-lg input-bordered border-gray-400 focus:outline-verde border-4 rounded-2xl input-primary w-full lg:max-w-xl" >
             </span>
             <span class="w-full space-y-2">
                 <h3>Mensaje:</h3>
-                <textarea bind:value={mensaje} class="textarea textarea-primary w-full lg:max-w-xl h-48 text-4xl lg:text-lg border-4 focus:outline-verde rounded-2xl border-gray-400" placeholder="Escribe aca tu mensaje"></textarea>
+                <textarea bind:value={mensaje} name="mensaje" class="textarea textarea-primary w-full lg:max-w-xl h-48 text-4xl lg:text-lg border-4 focus:outline-verde rounded-2xl border-gray-400" placeholder="Escribe aca tu mensaje"></textarea>
             </span>
             <button class="btn-normal text-black ">Enviar Mensaje</button>
-        </div>
+
+            <input type="hidden" name="_next" value="https://www.joeljmora.tech/tnx">
+        </form>
         <div class="flex lg:wa-1/2 px-2 max-lg:hidden h-full">
             <CodeMockup nombre={nombre} email={email} mensaje={mensaje}/>
         </div>
---
+++ src/routes/tnx.svelte
@@ -0,0 +1,2 @@
+<li><a href="https://www.joeljmora.tech/"></a></li>
+<h1>Gracias por darme las nalgas</h1>
